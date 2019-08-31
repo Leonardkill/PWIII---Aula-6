3f1ff84cfa5a0344d933ca71f99a86732c38a19c
Jacob <leonardofaria_7@hotmail.com>
2019-08-30 21:51:54 -0300

    Data binding Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'etec-base';
+  peso = 70;
+  altura = 1.70;
+  
+  calcular() : void {
+    let imc = this.peso / (this.altura * this.altura)
+    alert ("IMC é : " + imc)
+    
+  }
+
 }
